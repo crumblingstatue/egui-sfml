268891068d577af093776afa244724470b2bc938
crumblingstatue <radiantstatue@gmail.com>
2022-04-28 14:42:23 +0200

    demo: Use DemoWindows instead of WrapApp
    This allows usage without needing to get epi involved

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,37 +1,17 @@
-#![feature(new_uninit)]
-
-use std::rc::Rc;
-
-use egui_demo_lib::WrapApp;
+use egui_demo_lib::DemoWindows;
 use egui_sfml::SfEgui;
-use epi::{backend, App, IntegrationInfo};
 use sfml::{
     graphics::{Color, Rect, RenderTarget, RenderWindow, View},
     window::{Event, Style, VideoMode},
 };
 
 fn main() {
-    let mut app = WrapApp::default();
     let vm = VideoMode::desktop_mode();
     let mut rw = RenderWindow::new(vm, "Egui test", Style::NONE, &Default::default());
     rw.set_position((0, 0).into());
     rw.set_vertical_sync_enabled(true);
-    let mut frame = epi::Frame {
-        info: IntegrationInfo {
-            cpu_usage: None,
-            native_pixels_per_point: None,
-            prefer_dark_mode: None,
-            web_info: None,
-            name: "egui-sfml",
-        },
-        output: backend::AppOutput::default(),
-        storage: None,
-        gl: unsafe {
-            eprintln!("TODO: Fucked up workaround");
-            Rc::new_uninit().assume_init()
-        },
-    };
     let mut sfegui = SfEgui::new(&rw);
+    let mut demo = DemoWindows::default();
     while rw.is_open() {
         while let Some(ev) = rw.poll_event() {
             sfegui.add_event(&ev);
@@ -51,7 +31,7 @@ fn main() {
             }
         }
         sfegui.do_frame(|ctx| {
-            app.update(ctx, &mut frame);
+            demo.ui(ctx);
         });
         rw.clear(Color::BLACK);
         sfegui.draw(&mut rw, None);
