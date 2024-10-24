9c962b7feb528334bb7daab6afe8beec3d946e34
crumblingstatue <radiantstatue@gmail.com>
2024-10-24 05:10:14 +0200

    Put rendering into module called `rendering`

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,17 @@
 
 #![warn(missing_docs)]
 
+mod rendering;
+
 pub use {egui, sfml};
 use {
     egui::{
-        epaint::Primitive, Context, CursorIcon, Event as EguiEv, FullOutput, ImageData, Modifiers,
-        PointerButton, Pos2, RawInput, TextureId, ViewportCommand,
+        Context, CursorIcon, Event as EguiEv, FullOutput, Modifiers, PointerButton, Pos2, RawInput,
+        TextureId, ViewportCommand,
     },
     sfml::{
         cpp::FBox,
-        graphics::{
-            blend_mode::Factor, BlendMode, Color, PrimitiveType, RenderStates, RenderTarget,
-            RenderWindow, Texture, Vertex,
-        },
+        graphics::{RenderTarget as _, RenderWindow, Texture},
         system::{Clock, Vector2, Vector2i},
         window::{clipboard, mouse, Cursor, CursorType, Event, Key},
     },
@@ -368,7 +367,7 @@ impl SfEgui {
                     en.insert(tex)
                 }
             };
-            update_tex_from_delta(tex, delta)?;
+            rendering::update_tex_from_delta(tex, delta)?;
         }
         let new_cursor = match self.egui_result.platform_output.cursor_icon {
             CursorIcon::Default => Some(&self.cursors.arrow),
@@ -439,7 +438,7 @@ impl SfEgui {
         window: &mut RenderWindow,
         user_tex_src: Option<&mut dyn UserTexSource>,
     ) {
-        draw(
+        rendering::draw(
             window,
             &self.ctx,
             mem::take(&mut self.egui_result.shapes),
@@ -497,132 +496,3 @@ impl std::fmt::Display for PassError {
 }
 
 impl std::error::Error for PassError {}
-
-fn update_tex_from_delta(
-    tex: &mut Texture,
-    delta: &egui::epaint::ImageDelta,
-) -> Result<(), TextureCreateError> {
-    let mut x = 0;
-    let mut y = 0;
-    let [w, h] = delta.image.size();
-    if let Some([xx, yy]) = delta.pos {
-        x = xx as u32;
-        y = yy as u32;
-    }
-    match &delta.image {
-        ImageData::Color(color) => {
-            let srgba: Vec<u8> = color.pixels.iter().flat_map(|c32| c32.to_array()).collect();
-            unsafe {
-                tex.update_from_pixels(&srgba, w as u32, h as u32, x, y);
-            }
-        }
-        ImageData::Font(font_image) => {
-            let srgba: Vec<u8> = font_image
-                .srgba_pixels(None)
-                .flat_map(|c32| c32.to_array())
-                .collect();
-            if w > tex.size().x as usize || h > tex.size().y as usize {
-                // Resize texture
-                let ok = tex.create(w as u32, h as u32).is_ok();
-                if !ok {
-                    return Err(TextureCreateError {
-                        width: w,
-                        height: h,
-                    });
-                }
-            }
-            unsafe {
-                tex.update_from_pixels(&srgba, w as u32, h as u32, x, y);
-            }
-        }
-    }
-    Ok(())
-}
-
-fn draw(
-    window: &mut RenderWindow,
-    egui_ctx: &egui::Context,
-    shapes: Vec<egui::epaint::ClippedShape>,
-    user_tex_source: &mut dyn UserTexSource,
-    textures: &HashMap<TextureId, FBox<Texture>>,
-    pixels_per_point: f32,
-) {
-    let _ = window.set_active(true);
-    unsafe {
-        glu_sys::glEnable(glu_sys::GL_SCISSOR_TEST);
-    }
-    let mut vertices = Vec::new();
-    for egui::ClippedPrimitive {
-        clip_rect,
-        primitive,
-    } in egui_ctx.tessellate(shapes, pixels_per_point)
-    {
-        let mesh = match primitive {
-            Primitive::Mesh(mesh) => mesh,
-            _ => continue,
-        };
-        let (tw, th, tex) = match mesh.texture_id {
-            TextureId::Managed(id) => {
-                let tex = &*textures[&TextureId::Managed(id)];
-                let (egui_tex_w, egui_tex_h) = (tex.size().x as f32, tex.size().y as f32);
-                (egui_tex_w, egui_tex_h, tex)
-            }
-            TextureId::User(id) => user_tex_source.get_texture(id),
-        };
-        for idx in mesh.indices {
-            let v = mesh.vertices[idx as usize];
-            let sf_v = Vertex::new(
-                (v.pos.x, v.pos.y).into(),
-                Color::rgba(v.color.r(), v.color.g(), v.color.b(), v.color.a()),
-                (v.uv.x * tw, v.uv.y * th).into(),
-            );
-            vertices.push(sf_v);
-        }
-        let pixels_per_point = 1.;
-        let win_size = window.size();
-        let width_in_pixels = win_size.x;
-        let height_in_pixels = win_size.y;
-        // Code copied from egui_glium (https://github.com/emilk/egui)
-        // Transform clip rect to physical pixels:
-        let clip_min_x = pixels_per_point * clip_rect.min.x;
-        let clip_min_y = pixels_per_point * clip_rect.min.y;
-        let clip_max_x = pixels_per_point * clip_rect.max.x;
-        let clip_max_y = pixels_per_point * clip_rect.max.y;
-
-        // Make sure clip rect can fit within a `u32`:
-        let clip_min_x = clip_min_x.clamp(0.0, width_in_pixels as f32);
-        let clip_min_y = clip_min_y.clamp(0.0, height_in_pixels as f32);
-        let clip_max_x = clip_max_x.clamp(clip_min_x, width_in_pixels as f32);
-        let clip_max_y = clip_max_y.clamp(clip_min_y, height_in_pixels as f32);
-
-        let clip_min_x = clip_min_x.round() as u32;
-        let clip_min_y = clip_min_y.round() as u32;
-        let clip_max_x = clip_max_x.round() as u32;
-        let clip_max_y = clip_max_y.round() as u32;
-        unsafe {
-            glu_sys::glScissor(
-                clip_min_x as _,
-                (height_in_pixels - clip_max_y) as _,
-                (clip_max_x - clip_min_x) as _,
-                (clip_max_y - clip_min_y) as _,
-            );
-        }
-        let rs = RenderStates {
-            blend_mode: BlendMode {
-                color_src_factor: Factor::One,
-                color_dst_factor: Factor::OneMinusSrcAlpha,
-                alpha_src_factor: Factor::OneMinusDstAlpha,
-                alpha_dst_factor: Factor::One,
-                ..Default::default()
-            },
-            texture: Some(tex),
-            ..Default::default()
-        };
-        window.draw_primitives(&vertices, PrimitiveType::TRIANGLES, &rs);
-        vertices.clear();
-    }
-    unsafe {
-        glu_sys::glDisable(glu_sys::GL_SCISSOR_TEST);
-    }
-    let _ = window.set_active(false);
-}
